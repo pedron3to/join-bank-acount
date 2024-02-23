fcf46058cc29b94dc0a68847614abd09f4090156
Pedro Sartor <pedro.sartor@easylive.com.br>
2024-02-23 14:01:22 -0300

    script deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,29 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs/promises");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const bankAccount = await hre.ethers.deployContract("BankAccount");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // const signer = await bankAccount.signer.getAddress();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log(bankAccount);
 
-  await lock.waitForDeployment();
+  await bankAccount.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await writeDeploymentInfo(bankAccount);
+}
+
+async function writeDeploymentInfo(contract) {
+  const data = {
+    contract: {
+      address: contract.runner.address,
+      // signerAddress: contract.signer.address,
+      abi: contract.interface.format(),
+    },
+  };
+
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile("deployment.json", content, { encoding: "utf-8" });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
