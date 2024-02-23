a02e24178a0ae3f0905740cf69ff6e5c333c2347
Pedro Neto <pedro_neto@outlook.com>
2024-02-23 19:46:28 -0300

    change hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,6 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.19", // or a higher version
+    version: "0.8.17", // or a higher version
   },
 };
