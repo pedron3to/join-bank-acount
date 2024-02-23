9f3407dc4f2c19b6f1b1e6ab9dec1a56d8d69c53
Pedro Sartor <pedro.sartor@easylive.com.br>
2024-02-23 14:00:47 -0300

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.19", // or a higher version
+  },
 };
